7746a2d777e5e5180c878aaa34144e074a7d01eb
Brighting <ahzhangf@163.com>
2013-12-27 17:19:23 +0800

    add

---
+++ AutoLayout/AutoLayout/AutoLayout/BZViewController.h
@@ -0,0 +1,13 @@
+//
+//  BZViewController.h
+//  AutoLayout
+//
+//  Created by ZBright on 13-11-15.
+//  Copyright (c) 2013年 fnst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BZViewController : UIViewController
+
+@end
---
+++ OpenGL/HelloArrow/HelloArrow/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloArrow
+//
+//  Created by ZBright on 13-11-14.
+//  Copyright (c) 2013年 fnst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BZAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BZAppDelegate class]));
+    }
+}
